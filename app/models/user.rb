8385b00103a11a15850b72f8328bdc1da517d553
Miguel Ricaño <mricanho@gmail.com>
2021-02-16 20:30:05 -0600

    Add associations and validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :articles, foreign_key: :author_id, dependent: :destroy
+  has_many :votes, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 30 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
