19e71f4738821d3bfd0604fed033b1f8eb9dc799
Miguel Ricaño <mricanho@gmail.com>
2021-02-16 20:32:16 -0600

    Add category validations and associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,10 @@
 class Category < ApplicationRecord
+  has_and_belongs_to_many :articles
+  validates :name, presence: true, length: { mininum: 2, maximum: 20 }
+
+  scope :priority_order, -> { order(:priority) }
+
+  def most_recent_article
+    articles.most_recents.limit(1)
+  end
 end
