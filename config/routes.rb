ea25f355f5a3cd35171a7bf4c687a6bb93c25fe2
Miguel Ricaño <mricanho@gmail.com>
2021-02-17 12:31:51 -0600

    Add registrations controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :categories, only: %i[show]
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :articles, only: %i[new create] do 
     resources :votes, only: %i[create destroy]
   end
