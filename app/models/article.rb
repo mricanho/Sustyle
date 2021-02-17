99c5a74e497a9b2cb7d8896c6c6904f706dfb868
Miguel Ricaño <mricanho@gmail.com>
2021-02-16 20:25:37 -0600

    Add validations and associations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :votes, dependent: :destroy
+  has_and_belongs_to_many :categories
+  has_one_attached :image
+
+  validates :author_id
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
+  validates :text, presence: true
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
