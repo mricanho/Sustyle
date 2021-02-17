eb89bcc0c0df3bc14aad0023b10687af0e47097f
Miguel Ricaño <mricanho@gmail.com>
2021-02-17 14:25:07 -0600

    Add footer view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+
 
   # GET /articles or /articles.json
   def index
