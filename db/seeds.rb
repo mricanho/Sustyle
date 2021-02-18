73ba7100810d53b9cfe028d25d4044cbed87ec61
Miguel Ricaño <mricanho@gmail.com>
2021-02-17 22:00:03 -0600

    Add db seed, and modify articles controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'Fashion', priority: 1)
+Category.create(name: 'Technology', priority: 2)
+Category.create(name: 'Food', priority: 3)
+Category.create(name: 'Energy', priority: 4)
+Category.create(name: 'Economics', priority: 5)
+editor = User.create(name: "Editor", email: "sustyle_editor@gmail.com", password: "123456")
+tom = User.create(name: "Tom", email: "tom@gmail.com", password: "123456")
+
