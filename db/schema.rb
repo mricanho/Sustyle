0915627ca4512c34ee0f2c3224370534319c8273
Miguel Ricaño <mricanho@gmail.com>
2021-02-17 20:09:44 -0600

    Install carrierwave and add column image to article, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_021230) do
+ActiveRecord::Schema.define(version: 2021_02_18_020814) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_021230) do
     t.integer "votes_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
