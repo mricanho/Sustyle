c3660bee715248683a8b9d286e86280a54d0b0f2
Miguel Ricaño <mricanho@gmail.com>
2021-02-19 09:40:00 -0600

    Include ImageUploader in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
+  include ImageUploader::Attachment(:image)
   belongs_to :author, class_name: 'User'
   has_many :votes, dependent: :destroy
   has_and_belongs_to_many :categories
-  has_one_attached :image
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
   validates :text, presence: true
