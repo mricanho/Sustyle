3eae2dd8f506e00733690949e5b935f97248ad20
Miguel Ricaño <mricanho@gmail.com>
2021-02-19 23:04:59 -0600

    Add .env to .gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,15 @@ Category.create(name: 'Food', priority: 3)
 Category.create(name: 'Energy', priority: 4)
 Category.create(name: 'Economics', priority: 5)
 editor = User.create(name: "Editor", email: "sustyle_editor@gmail.com", password: "123456")
-tom = User.create(name: "Tom", email: "tom@gmail.com", password: "123456")
+tom = User.create(name: "Tom", email: "tom@gmail.com", password: "password1234")
+kimberly = User.create(name: "Kimberly", email: "kim@gmail.com", password: "password1234")
+mike = User.create(name: "Mike", email: "mike@gmail.com", password: "password1234")
+tim = User.create(name: "Tim", email: "tim@gmail.com", password: "password1234")
+
+Article.create(author_id:2, title: "Tesla's Cheap $25,000 Car Could Cost Just $19,000", text: "YouTuber Andy Slye optimistically explains how the upcoming cheap $25,000 Tesla could only cost $19,000, and possibly kill off the gas competition. At the beginning of the video he warns us that he is being very optimistic about all this, but it could happen if the timelines lineup. 
+
+  He starts with explaining how Elon Musk always wanted a high volume affordable EV. In his Master Plan from way back in 2006, he outlined how he wanted a low volume expensive car (Roadster) to fund a medium volume car at lower price (Model S). Then use the money from that to fund the creation of a high volume affordable car (Model 3 and/or 2?). The tweet below gives you a good idea of what he is talking about. 
+  
+  ",
+  image_data: "id: '5ec2dd05abff2f4c0c4be5d9fbc82958.jpg'"
 
