78be8744ba3f719647157d272e451888730627d1
Miguel Ricaño <mricanho@gmail.com>
2021-02-21 20:07:19 -0600

    Finish categories page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,17 @@
 module ApplicationHelper
-  # def four_articles(article, index)
-  #   if index < 2
-  #     content_tag(:div, nil, class: 'column is-gapless') do
-  #       content_tag(:div, nil, class: 'photo first column is-one-quarter') do
-  #         display_photo(article)
-  #       end +
-  #         content_tag(:div, nil, class: 'details column card is-one-quarter') do
-  #           display_details(article)
-  #         end
-  #     end
-  #   else
-  #     content_tag(:div, nil, class: 'columns is-gapless') do
-  #       content_tag(:div, nil, class: 'details card column') do
-  #         display_details(article)
-  #       end +
-  #         content_tag(:div, nil, class: 'photo second column') do
-  #           display_photo(article)
-  #         end
-  #     end
-  #   end
-  # end
-
-  def two_articles1(article, index)
+  def four_articles(article, index)
     if index < 2
-      content_tag(:div, nil, class: 'column is-half') do
-        content_tag(:div, nil, class: 'photo first column is-one-quarter') do
-          display_photo(article)
-        end +
-          content_tag(:div, nil, class: 'details column card is-one-quarter') do
-            display_details(article)
-          end
-      end
+      content_tag(:div, nil, class: 'photo', style: "background: url('#{display_photo(article)}') center center; background-size: cover") do
+      end +
+        content_tag(:div, nil, class: 'details', style: "background-color: white") do
+          display_details(article)
+        end
+    else
+      content_tag(:div, nil, class: 'details', style: "background-color: white") do
+        display_details(article)
+      end +
+        content_tag(:div, nil, class: 'photo', style: "background: url('#{display_photo(article)}') center center; background-size: cover") do
+        end
     end
   end
 
@@ -38,29 +19,37 @@ module ApplicationHelper
     if article.image_url
       article.image_url
     else
-      "https://source.unsplash.com/1600x900/?{#{article.title}}"
+      image_tag("https://source.unsplash.com/1600x900/?{#{article.title}}")
     end
   end
 
-  def display_details(art)
-    content_tag(:h1, @category.name, class: 'orange-text') + content_tag(:h2, art.title) +
-      content_tag(:p, simple_format(art.text.truncate(140))) +
-      content_tag(:span, "#{art.votes_count} votes - Your reaction: ") +
-      content_tag(:span, nil, class: 'orange-links') do
-        vote_toggle_btn(art)
-      end + content_tag(:br) + content_tag(:span, "Written by #{art.author.name.capitalize}")
+  def display_photo1(article)
+    if article.image_url
+      image_tag(article.image_url)
+    else
+      image_tag("https://source.unsplash.com/1600x900/?{#{article.title}}")
+    end
   end
 
-  def vote_toggle_btn(art)
+  def display_details(article)
+    content_tag(:h1, @category.name, class: 'orange hero-head title is-size-4') + content_tag(:h2, article.title, class: 'title is-size-4') +
+      content_tag(:p, simple_format(article.text.truncate(105), class: "hero-body px-0")) +
+      content_tag(:span, "#{article.votes_count} votes - If you like this article, show it:  ", class: "pt-3") +
+      content_tag(:span, nil, class: 'orange-links button is-small') do
+        vote_button(article)
+      end + content_tag(:br) + content_tag(:span, "By #{article.author.name.capitalize}", class: "orange")
+  end
+
+  def vote_button(art)
     if current_user
       vote = Vote.find_by(article: art, user: current_user)
       if vote
-        link_to('Click to Unvote', article_vote_path(id: vote.id, article_id: art.id), method: :delete)
+        link_to('Click to Unvote!', article_vote_path(id: vote.id, article_id: art.id), method: :delete)
       else
         link_to('Click to Vote!', article_votes_path(article_id: art.id), method: :post)
       end
     else
-      link_to('Log in to vote', log_in_path)
+      link_to('LOG IN to vote!', user_session_path)
     end
   end
 
@@ -72,7 +61,7 @@ module ApplicationHelper
         concat link_to(
           content_tag(:div, nil, class: 'hero-head ml-5 mt-4') do
             content_tag(:p, @categories.find(index + 1).name, class: "title has-text-white") + content_tag(:p, nil, class: "hero-body pt-6 has-text-white") + (content_tag(:p, articles.title, class: "hero-foot subtitle size-6 mb-2") if articles)
-          end + ".", '#', class: 'has-text-black column', style: "background: url('#{display_photo(articles) if articles}') center center; background-size: cover"
+          end + "", '#', class: 'has-text-black column', style: "background: url('#{display_photo(articles) if articles}') center center; background-size: cover"
         )
       end
     end    
@@ -80,4 +69,3 @@ module ApplicationHelper
 
   
 end
-# (display_photo(articles) if articles)
