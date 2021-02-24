4b88709b2d4d2338ca0dc64b0774cff29ca80661
Miguel Ricaño <mricanho@gmail.com>
2021-02-24 12:48:32 -0600

    Create article show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :categories, only: %i[show new create index]
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :articles, only: %i[new create destroy show index] do 
+  resources :articles, only: %i[new create destroy show index edit] do 
     resources :votes, only: %i[create destroy]
   end
   
