c0c7adbc48a9ee8748fa12d1fbf079af9b44d7e7
Miguel Ricaño <mricanho@gmail.com>
2021-08-25 11:54:56 -0500

    Add routes for 2FA

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   resources :categories, only: %i[show new create index]
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations'. sessions: 'users/sessions'}
+  # Two factor add
+  devise_scope :user do
+    scope :users, as: :users do
+      post 'pre_otp', to: 'users/sessions#pre_otp'
+    end
+  end
+
+  resource :two_factor
+
+  # end
+
   resources :articles, only: %i[new create destroy show index edit] do 
     resources :votes, only: %i[create destroy]
   end
